bd258ba8ef6c52c7e0f545574b259b40a31f7a29
KaylinAnn <40373229+KaylinAnn@users.noreply.github.com>
2019-03-04 13:46:26 -0700

    set up doors with buttons and styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1 className="title">Monty Hall's Paradox</h1>
+        <div className="doors-container">
+          <div className="door-one doors" />
+          <div className="door-two doors" />
+          <div className="door-three doors"> </div>
+        </div>
+        <div className="button-container">
+          <button className="button-door-one door-buttons">DOOR ONE</button>
+          <button className="button-door-two door-buttons">DOOR TWO</button>
+          <button className="button-door-three door-buttons">DOOR THREE</button>
+        </div>
       </div>
     );
   }
